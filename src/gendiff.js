56f4abca935b218b3e1e5d83b89f2aa9cc7fad69
jose-lop <jose.ant1022@gmail.com>
2025-11-25 05:56:17 -0500

    Fix buildTree import and restore working tests

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,18 +1,26 @@
+// src/gendiff.js
 import fs from 'fs';
 import path from 'path';
 import parse from './parsers.js';
-import buildDiff from './buildDiff.js';
+import buildTree from './buildTree.js';
 import format from './formatters/index.js';
 
-const getData = (filepath) =>
-  fs.readFileSync(path.resolve(filepath), 'utf-8');
+const getData = (filepath) => {
+  const absolutePath = path.resolve(process.cwd(), filepath);
+  const content = fs.readFileSync(absolutePath, 'utf8');
+  const ext = path.extname(filepath).slice(1); // sin el punto
+  return parse(content, ext);
+};
 
-const getExt = (filepath) => path.extname(filepath).slice(1);
+const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const data1 = getData(filepath1);
+  const data2 = getData(filepath2);
 
-const gendiff = (file1, file2, formatName = 'stylish') => {
-  const data1 = parse(getData(file1), getExt(file1));
-  const data2 = parse(getData(file2), getExt(file2));
-  const diff = buildDiff(data1, data2);
+  const diff = buildTree(data1, data2);
+
+  // AQUÍ estaba el error
+  // Alguien en tu código actual está llamando: format(diff, filepath2)
+  // Esto lo arreglamos así:
   return format(diff, formatName);
 };
 
